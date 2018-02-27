1c2467a6f27c38b74af5d8931521a0c1dcedc849
an0n1m0us <33968273+an0n1m0us@users.noreply.github.com>
2018-02-27 08:46:53 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,17 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+const config = require('./config.json');
 
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {// crie uma pasta chamada events
+    let eventFunction = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+client.on(eventName, (...args) => eventFunction.run(client, ...args));
+  });
+});
 client.on("message", message => {
   const prefix = "ra!"; 
   if (message.author.bot) return;
