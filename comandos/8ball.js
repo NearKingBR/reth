3fa594218bbf865c843062d90221bf65f1ca6add
an0n1m0us <33968273+an0n1m0us@users.noreply.github.com>
2018-03-05 07:59:00 -0300

    Delete 8ball.js

--- comandos/8ball.js
+++
@@ -1,8 +0,0 @@
-exports.run = (client, message, args) => {
-   {
-  var texto = [':8ball:Sim...', ':8ball:Não!',  ':8ball:talvez...', ':8ball:não sei...'];
-      const random = texto[Math.floor(Math.random() * texto.length)];
-    message.reply(random)
-    message.react('🎱')
-    }
-  }; 
