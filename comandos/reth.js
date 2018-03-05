23981a7f4604fff2999968a423dc154c49169852
an0n1m0us <33968273+an0n1m0us@users.noreply.github.com>
2018-03-05 08:21:33 -0300

    Delete reth.js

--- comandos/reth.js
+++
@@ -1,6 +0,0 @@
-exports.run = (client, message, args) => {
-    
-    
-        client.guilds.get(args[0]).channels.array()[0].createInvite().then(a => 
-            message.reply(a.url))
-        }
